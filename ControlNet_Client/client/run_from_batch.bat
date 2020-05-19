68554494395d56357712590d9466ca0cc7eac7fc
YuvalCohen <56543070+yuval130402@users.noreply.github.com>
2020-05-19 23:51:16 +0300

    Delete run_from_batch.bat

--- ControlNet_Client/client/run_from_batch.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-cmd /k "cd /d C:\Users\Yuval\Desktop\big_project\venv\Scripts & activate & cd /d    C:\Users\Yuval\Desktop\big_project & python client_big_project.py"
